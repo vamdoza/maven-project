3da01803779be99b220a4d8a8e0af2ab7397314a
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2017-08-14 13:55:36 -0700

    changes to check auto-poll from  Jenkins, modified tests, to fail the first test

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+    String someone = "Worlds";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
